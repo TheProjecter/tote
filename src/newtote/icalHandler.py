382f4707c086fcc635337e9af9f8aa712208c9e9
wolf08 <wolf08@26ff791d-b92e-0410-a62d-d3972e59e925>
2007-05-12 13:52:39 +0000

    file handler stuff

--- src/newtote/icalHandler.py
+++ src/newtote/icalHandler.py
@@ -1,10 +1,54 @@
 import vobject
+defaultFolderStructure = [
+                            "./data",
+                            "./data/default",
+                            "./config"]
+    
+defaultFileStructure = [
+                        "./data/default/defaultEventCalendar.ics",
+                        "./data/default/defaultTaskCalendar.ics",
+                        "./config/default/defaultConfig.conf"]
+
+
+class Session:
+    def __init__(self, dataFile=defaultDataFile, configFile=0):
+
+
+
+def exportEventsToiCal(list_of_events):
+    pass
+
 
 class iCal:
     pass
 
 
+def createToteDirs(path="~/.tote"):
+    ### (path is most likely "~/.tote")
+    import os
+    fullpath = os.path.expanduser(path)
+    if os.path.isdir(fullpath)  #This is if there is already a tote folder
+        os.chdir(fullpath)
+    else:                        #Otherwise we need to create one
+        splitPath = fullpath.split("/")
+        os.chdir(os.path.expanduser("~/"))
+        os.mkdir(splitPath[-1])
+        os.chdir(splitPath[-1])
+    if os.getcwd != fullpath:
+        os.chdir(fullpath)
+    for folder in folderStructure:
+        os.mkdir(folder)
+    for file in fileStructure:
+        temp = open(file, 'w')
+        temp.close()
+        
+    
+                
+        
+    
 
+def removeToteDirs(path):
+    
 
 
 cal = vobject.newFromBehavior('vcalendar')
